5d67360e30788db990c7f4d8111e541eec018984
omerbeithalahmy <omer24883@gmail.com>
2026-03-22 12:31:05 +0200

    MVP

---
+++ script.js
@@ -0,0 +1,22 @@
+// Mobile menu
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+// Close navbar when link is clicked
+document.querySelectorAll(".nav-link").forEach((link) => {
+  link.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  });
+});
+
+// Year in footer
+const dateEl = document.getElementById("datee");
+if (dateEl) {
+  dateEl.textContent = new Date().getFullYear();
+}
