3d9f6fc698f87d1be2ef8aaa273352e60f4dc014
omerbeithalahmy <omer24883@gmail.com>
2026-03-22 14:12:05 +0200

    Arrow icon - for scroll up

--- script.js
+++ script.js
@@ -58,3 +58,21 @@ if (contactForm) {
     }
   });
 }
+
+// Scroll to Top
+const scrollTopBtn = document.getElementById("scroll-top");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    scrollTopBtn.classList.add("show");
+  } else {
+    scrollTopBtn.classList.remove("show");
+  }
+});
+
+scrollTopBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
